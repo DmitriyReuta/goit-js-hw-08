477fe3aaa950f19b9297da8fe30f671dae56af0c
Dmitriy Reuta <reutadmitriy@gmail.com>
2023-08-13 00:03:28 +0300

    first one

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
+import throttle from 'lodash.throttle'; 
+const iframe = document.querySelector("iframe")
+const framePlay = new Player(iframe);
 
+
+const timePlay = function (data) {
+    let time = data.seconds
+    localStorage.setItem("videoplayer-current-time", time);
+}
+
+const storedTime = localStorage.getItem("videoplayer-current-time");
+
+function errorFn() {
+    alert("Error!");
+}
+
+if (storedTime) {
+    framePlay.setCurrentTime(storedTime).catch(errorFn);
+}
+framePlay.on('timeupdate', throttle(timePlay, 1000));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+const emailInput = feedbackForm.querySelector('[name="email"]');
+const messageInput = feedbackForm.querySelector('[name="message"]');
+
+const saveFormState = throttle(() => {
+    const formState = {
+        email: emailInput.value,
+        message: messageInput.value,
+    };
+    localStorage.setItem("feedback-form-state", JSON.stringify(formState));
+}, 500)
+
+feedbackForm.addEventListener('input', saveFormState);
+const storedFormState = localStorage.getItem("feedback-form-state");
+if (storedFormState) {
+    const parsedFormState = JSON.parse(storedFormState);
+    emailInput.value = parsedFormState.email || '';
+    messageInput.value = parsedFormState.message || '';
+}
+
+feedbackForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+console.log(formState);
+    
+emailInput.value = '';
+messageInput.value = '';
+localStorage.removeItem('feedback-form-state');
+})
